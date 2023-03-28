bb97c6dec529710ecff9a41a0ce7f8d13fb44913
Roman <im.roman996@gmail.com>
2023-03-28 16:28:06 +0300

    Added Car Migrations

---
+++ Shop/ShopTARgv21.Data/Migrations/20230328132041_car.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopTARgv21.Data.Migrations
+{
+    public partial class car : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfRegistration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VINCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fuel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    NumberOfCarDoors = table.Column<int>(type: "int", nullable: false),
+                    NumberOfPassangersWithDriver = table.Column<int>(type: "int", nullable: false),
+                    CarWeight = table.Column<int>(type: "int", nullable: false),
+                    BuildOfDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateOfRegistration = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FileToDatabase",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageData = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    CarId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileToDatabase", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FileToDatabase_Car_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Car",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileToDatabase_CarId",
+                table: "FileToDatabase",
+                column: "CarId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileToDatabase");
+
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
