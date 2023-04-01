0b474dcb1e1a1f21c2a3b35e0529d3077d0b1db5
Roman <im.roman996@gmail.com>
2023-04-01 18:18:12 +0300

    Added application Services

---
+++ Shop/ShopTargv21.ApplicationServices/Services/OpenWeatherServices.cs
@@ -0,0 +1,33 @@
+﻿using Nancy.Json;
+using ShopTARgv21.Core.Dto.OpenWeather;
+using ShopTARgv21.Core.ServiceInterface;
+using System.Net;
+
+
+namespace ShopTARgv21.ApplicationServices.Services
+{
+    public class OpenWeatherServices : IOpenWeatherServices
+    {
+        public async Task<OpenWeatherResultDto> OpenWeatherDetail(OpenWeatherResultDto dto)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?lat=59.26&lon=24.45&appid=c7a2fb774f74131b9e9824c292100f6b&units=metric";
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                Root weatherInfo = (new JavaScriptSerializer()).Deserialize<Root>(json);
+
+                dto.temp = weatherInfo.main.temp;
+                dto.feels_like = weatherInfo.main.feels_like;
+                dto.pressure = weatherInfo.main.pressure;
+                dto.humidity = weatherInfo.main.humidity;
+
+                dto.main = weatherInfo.weather[0].main;
+                dto.speed = weatherInfo.wind.speed;
+
+                var jsonString = new JavaScriptSerializer().Serialize(dto);
+            }
+            return dto;
+        }
+    }
+}
